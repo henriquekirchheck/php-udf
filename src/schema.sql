cdbeb093cef1f8a02ffcdbc6f0144ca833b8897a
Henrique Kirch Heck <me@henriquekh.dev.br>
2025-10-13 17:56:02 -0300

    postgres

--- src/schema.sql
+++ src/schema.sql
@@ -1,10 +1,10 @@
 CREATE TABLE student (
-  cpf CHAR(11) PRIMARY KEY CONSTRAINT cpf_format CHECK(VALUE ~ '^[[:digit:]]{11}$'),
+  cpf CHAR(11) PRIMARY KEY CONSTRAINT cpf_format CHECK(cpf ~ '^[[:digit:]]{11}$'),
   first_name text NOT NULL,
   last_name text NOT NULL,
-  full_name text GENERATED ALWAYS AS (concat_ws(' ', first_name, last_name)) STORED,
+  full_name text GENERATED ALWAYS AS (first_name || ' ' || last_name) STORED,
   birthDate date NOT NULL,
   address text NOT NULL,
-  ddd text NOT NULL CONSTRAINT ddd_format CHECK(VALUE ~ '^[[:digit:]]{2}$'),
-  phoneNumber text NOT NULL CONSTRAINT phone_format CHECK(VALUE ~ '^[[:digit:]]{9}$')
+  ddd text NOT NULL CONSTRAINT ddd_format CHECK(ddd ~ '^[[:digit:]]{2}$'),
+  phoneNumber text NOT NULL CONSTRAINT phone_format CHECK(phoneNumber ~ '^[[:digit:]]{9}$')
 );
